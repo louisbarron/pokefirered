1c0cc5b96cf67ab29caaf5fc386909c24dfa7f59
louisbarron <35508893+louisbarron@users.noreply.github.com>
2024-08-07 14:33:10 +1200

    update test pokemon

--- src/data/pokemon/pokedex_text.h
+++ src/data/pokemon/pokedex_text.h
@@ -1,5 +1,15 @@
-#if defined(FIRERED)
-#include "pokedex_text_fr.h"
-#elif defined(LEAFGREEN)
-#include "pokedex_text_lg.h"
-#endif
+const u8 gDummyPokedexText[] = _(
+    "This is a newly discovered POKéMON. It is\n"
+    "currently under investigation. No detailed\n"
+    "information is available at this time.");
+
+const u8 gDummyPokedexTextUnused[] = _("");
+
+const u8 gTestPokedexText[] = _("This is for testing\nA testing pokemon.");
+
+const u8 gTestPokedexTextUnused[] = _("");
+
+const u8 gTesttwoPokedexText[] = _("This is another test for testing\nA testing pokemon.\nFurret.");
+
+const u8 gTesttwoPokedexTextUnused[] = _("");
+
